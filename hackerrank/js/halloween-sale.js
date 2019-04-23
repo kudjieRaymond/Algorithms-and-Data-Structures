38ec781db34f7a10c090a6948d73a10a774eea59
Ray Kudjie <rkudjie@gmail.com>
2019-04-23 11:40:00 +0000

    Hackerrank Halloween-sale challenge

--- hackerrank/js/halloween-sale.js
+++ hackerrank/js/halloween-sale.js
@@ -3,3 +3,22 @@
 https://www.hackerrank.com/challenges/halloween-sale * 
  *
 **/
+
+function howManyGames(p, d, m, s) {
+    // Return the number of games you can buy
+    
+    let amount_left = s
+    let count = 0
+    let current_price = p
+    
+    while(s >= p && amount_left >= current_price){
+        
+        amount_left -= current_price
+        count ++
+        current_price = (current_price - d > m ) ? current_price -d : m
+        
+    }
+    
+    return count
+
+}
